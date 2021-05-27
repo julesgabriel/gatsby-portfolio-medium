acd5c06e76140dcb80029fa5f120376ca6b5e866
Jules DAYAUX <julesdayaux@MacBook-Pro-de-Jules.local>
2021-05-27 22:49:02 +0200

    UPDATE cookie bar disabled

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
   siteLanguage: "fr_FR",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
   seoTitleSuffix: "Portfolio Jules DAYAUX", // SEO title syntax will be e.g. "Imprint - {seoTitleSuffix}"
-  useCookieBar: true, // If you use Google Analytics and want to be GDPR-compliant, set it to true
+  useCookieBar: false, // If you use Google Analytics and want to be GDPR-compliant, set it to true
   googleAnalyticsTrackingId: "", // e.g. UA-XXXXXX-X
 
   // -- THEME SETTINGS -----
