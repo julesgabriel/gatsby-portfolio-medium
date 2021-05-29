db60a85652a2b8ec73087b133789734bc307ba96
Jules DAYAUX <julesdayaux@mbp-de-jules.home>
2021-05-30 00:41:21 +0200

    UPDATE metadata

--- config/index.js
+++ config/index.js
@@ -4,7 +4,7 @@ module.exports = {
   siteTitle: "Portfolio - Dayaux Jules | Développeur web",
   siteShortTitle: "Gatsby", // Used as logo text in header, footer, and splash screen
   siteDescription:
-    "Portfolio minimaliste de Jules DAYAUX - Alternant, Développeur et freelance dans le milieu du digital et du web de manière général",
+    "Portfolio minimaliste de Jules DAYAUX - Alternant, Développeur et freelance dans le digital. Je partage mes connaissances aux professionnels du digital qui souhaite en apprendre plus. J'accompagne aussi mes clients dans leurs projets les plus ambitieux",
   siteUrl: "https://julesdayaux.fr/",
   siteLanguage: "fr_FR",
   siteIcon: "content/favicon.png", // Relative to gatsby-config file
